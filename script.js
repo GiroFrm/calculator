267caa5effb6b5dcc20b2e9edd0f945fd53a11cf
GiroFrm <girofram844@gmail.com>
2024-10-11 21:14:47 +1100

    feat:Implement basic arithmetic functions and interactive calculator logic
    - Added basic arithmetic functions: add, subtract, multiply, and divide
    - Implemented operate function to handle different arithmetic operations based on operator parameter
    - Created display and clear functionality for calculator
    - Added event listeners for number buttons and clear button
    - Updated display logic to show number inputs in the display field

--- script.js
+++ script.js
@@ -34,4 +34,30 @@ function operate(a, operator, b) {
     }
     return result;
 }
+let arrayValues = [];
+
+const display = document.querySelector(".display");
+const btnNumber = document.querySelectorAll(".number");
+const btnAC = document.getElementById("btn-ac");
+
+      btnNumber.forEach(btn => btn.addEventListener("click", ()=>{
+        let value =   btn.value;
+        displayNumber(value);
+      }))
+      
+      btnAC.addEventListener("click", ()=>{
+        clearDisplay();
+      })
+
+   
+function displayNumber(number){
+   // display number to input display
+   arrayValues.push(number);
+   display.value = arrayValues.join("");
+}
+
+function clearDisplay() {
+    display.value= "";
+    arrayValues = [];
+}
 
