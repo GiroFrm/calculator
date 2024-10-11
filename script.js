c39d16c6c0110e0570771a85df9b7667d2d47f1b
GiroFrm <girofram844@gmail.com>
2024-10-11 17:39:24 +1100

    feat: Add operate function to perform basic aritmetic operations
    - implement operate function to handle addition, subtraction, multiplication, and division based on the operation parameter
    - Utilize existing add, subtract, multiply, and divide functions for operation logic
    - Included error handling for invalid operations

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, operator, b) {
+    let result=0;
+    switch(operator) {
+        case '+':
+            result = add(a,b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+        default:
+            console.log('ERROR');
+    }
+    return result;
+}
+
